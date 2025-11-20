fd40163f4df7a27156467a898d6b36335d6139db
Alexander Suetin <asuetin@gpmd.ru>
2025-11-20 14:34:15 +0800

    feat: 3000 port

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   build: {
     rollupOptions: {
       input: {
